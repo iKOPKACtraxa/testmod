c6e4a06e843a4c9c86d8931a5d8e930e9b2faad6
iKOPKACtraxa <73790456+iKOPKACtraxa@users.noreply.github.com>
2021-02-14 22:04:19 +0300

    Update testmod.go

--- testmod.go
+++ testmod.go
@@ -1,16 +1,22 @@
-package main
+package testmod
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/iKOPKACtraxa/testmod/v2"
 )
 
-func main() {
-	// fmt.Println(testmod.Hi("roberto"))
-	g, err := testmod.Hi("Roberto", "pt")
-	if err != nil {
-		panic(err)
+// Hi returns a friendly greeting in language lang
+func Hi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "pt":
+		return fmt.Sprintf("Oi, %s!", name), nil
+	case "es":
+		return fmt.Sprintf("¡Hola, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", errors.New("unknown language")
 	}
-	fmt.Println(g)
 }
